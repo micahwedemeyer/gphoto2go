cfb81ffe95f8948f5995008d8b91f78f73f286c2
Micah Wedemeyer <me@micahwedemeyer.com>
2015-07-06 12:15:06 -0400

    Read files from the camera

--- README.md
+++ README.md
@@ -6,16 +6,17 @@ A more Go idiomatic interface to the gPhoto2 library.
 
 I'm not a great Golang programmer, so this could be a complete disaster. But, if you want to use the gPhoto2 library and you'd prefer to avoid sticking cgo references all over your main Go program, this library might help.
 
+I'm also not a great C programmer, having grown up with garbage collectors and other niceties. Therefore, this library will be riddled with memory leaks. I would appreciate any help in making it more memory efficient.
+
 ## Installation
 
-`go get github.com/micahwedemeyer/gphoto2go`
-
+    go get github.com/micahwedemeyer/gphoto2go
 
 ## Requirements
 
 You will also need libgphoto2 installed. If you are on Mac OS X, I recommend installing it with homebrew.
 
-`brew install libgphoto2`
+    brew install libgphoto2
 
 In order to compile your Go program, you will need to set CFLAGS and LDFLAGS to find the libgphoto2 libraries. I will update with more instructions on that once I understand, but the following worked for me with the homebrew-installed libgphoto2
 
@@ -32,18 +33,27 @@ The main goal with this library is to present a Go-friendly interface to the C m
     camera := new(gphoto2.Camera)
     err := camera.Init()
 
-    if err < 0 {
-        fmt.Printf(gphoto2.CameraResultToString(err))
-    }
 
 This will create a new Camera struct and intitialize it, which prompts gphoto2 to auto-detect any connected USB cameras.
 
 ### Taking a Photo
 
-    camera.trigger_capture()
+    camera.TriggerCapture()
 
 This will trigger the camera.
 
 ### Downloading the Photos from the Camera
 
-Coming soon...
+    cameraFileReader := camera.BufferedFileReader("/store_00020001/DCIM/100CANON", "IMG_8085.JPG")
+    fileWriter := os.Create("/tmp/myfile.jpg")
+    io.Copy(fileWriter, cameraFileReader)
+
+### Interpreting errors
+
+Most of the functions will return an error code. If it is less than zero, that means an error has occurred. The library can translate the error integer
+into a human readable string.
+
+    err := camera.TriggerCapture()
+    if err < 0 {
+        fmt.Printf(gphoto2.CameraResultToString(err))
+    }
--- gphoto2.go
+++ gphoto2.go
@@ -3,10 +3,13 @@ package gphoto2
 // #cgo CFLAGS: -I/Users/micah/Developer/include/gphoto2
 // #cgo LDFLAGS: -L/Users/micah/Developer/lib -lgphoto2
 // #include <gphoto2.h>
-// #include <gphoto2/gphoto2-version.h>
+// #include <stdlib.h>
 import "C"
 import "unsafe"
 import "fmt"
+import "strings"
+import "io"
+import "bufio"
 
 type Camera struct {
 	camera  *C.Camera
@@ -27,6 +30,11 @@ func (c *Camera) GetAbilities() (C.CameraAbilities, int) {
 	return abilities, int(err)
 }
 
+func (c *Camera) TriggerCapture() int {
+	err := C.gp_camera_trigger_capture(c.camera, c.context)
+	return int(err)
+}
+
 func (c *Camera) WaitForCameraEvent(timeout int, handler func(int, string)) {
 	var eventType C.CameraEventType
 	var vp unsafe.Pointer
@@ -40,6 +48,101 @@ func (c *Camera) WaitForCameraEvent(timeout int, handler func(int, string)) {
 	handler(int(eventType), s)
 }
 
+func (c *Camera) ListFolders(folder string) ([]string, int) {
+	if folder == "" {
+		folder = "/"
+	}
+
+	var cameraList *C.CameraList
+	C.gp_list_new(&cameraList)
+	defer C.free(unsafe.Pointer(cameraList))
+
+	cFolder := C.CString(folder)
+	defer C.free(unsafe.Pointer(cFolder))
+
+	err := C.gp_camera_folder_list_folders(c.camera, cFolder, cameraList, c.context)
+	folderMap, _ := cameraListToMap(cameraList)
+
+	names := make([]string, len(folderMap))
+	i := 0
+	for key, _ := range folderMap {
+		names[i] = key
+		i += 1
+	}
+
+	return names, int(err)
+}
+
+func (c *Camera) RListFolders(folder string) []string {
+	folders := make([]string, 0)
+	path := folder
+	if !strings.HasSuffix(path, "/") {
+		path = path + "/"
+	}
+	subfolders, _ := c.ListFolders(path)
+	for _, sub := range subfolders {
+		subPath := path + sub
+		folders = append(folders, subPath)
+		folders = append(folders, c.RListFolders(subPath)...)
+	}
+
+	return folders
+}
+
+func (c *Camera) ListFiles(folder string) ([]string, int) {
+	if folder == "" {
+		folder = "/"
+	}
+
+	if !strings.HasSuffix(folder, "/") {
+		folder = folder + "/"
+	}
+
+	var cameraList *C.CameraList
+	C.gp_list_new(&cameraList)
+	defer C.free(unsafe.Pointer(cameraList))
+
+	cFolder := C.CString(folder)
+	defer C.free(unsafe.Pointer(cFolder))
+
+	err := C.gp_camera_folder_list_files(c.camera, cFolder, cameraList, c.context)
+	fileNameMap, _ := cameraListToMap(cameraList)
+
+	names := make([]string, len(fileNameMap))
+	i := 0
+	for key, _ := range fileNameMap {
+		names[i] = key
+		i += 1
+	}
+
+	return names, int(err)
+}
+
+func cameraListToMap(cameraList *C.CameraList) (map[string]string, int) {
+	size := int(C.gp_list_count(cameraList))
+	vals := make(map[string]string)
+
+	if size < 0 {
+		return vals, size
+	}
+
+	for i := 0; i < size; i++ {
+		var cKey *C.char
+		var cVal *C.char
+
+		C.gp_list_get_name(cameraList, C.int(i), &cKey)
+		C.gp_list_get_value(cameraList, C.int(i), &cVal)
+		defer C.free(unsafe.Pointer(cKey))
+		defer C.free(unsafe.Pointer(cVal))
+		key := C.GoString(cKey)
+		val := C.GoString(cVal)
+
+		vals[key] = val
+	}
+
+	return vals, 0
+}
+
 func (c *Camera) Model() (string, int) {
 	abilities, err := c.GetAbilities()
 	modelBytes := C.GoBytes(unsafe.Pointer(&abilities.model), 255)
@@ -50,4 +153,99 @@ func (c *Camera) Model() (string, int) {
 
 func CameraResultToString(err int) string {
 	return C.GoString(C.gp_result_as_string(C.int(err)))
+}
+
+// Need to find a good buffer size
+// For now, let's try 1MB
+const fileReaderBufferSize = 1 * 1024 * 1024
+
+type cameraFileReader struct {
+	camera   *Camera
+	folder   string
+	fileName string
+	fullSize uint64
+	offset   uint64
+
+	buffer [fileReaderBufferSize]byte
+}
+
+func (cfr *cameraFileReader) Read(p []byte) (int, error) {
+	n := uint64(len(p))
+
+	if n == 0 {
+		return 0, nil
+	}
+
+	bufLen := uint64(len(cfr.buffer))
+	remaining := cfr.fullSize - cfr.offset
+
+	toRead := bufLen
+	if toRead > remaining {
+		toRead = remaining
+	}
+
+	if toRead > n {
+		toRead = n
+	}
+
+	cFileName := C.CString(cfr.fileName)
+	cFolderName := C.CString(cfr.folder)
+	defer C.free(unsafe.Pointer(cFileName))
+	defer C.free(unsafe.Pointer(cFolderName))
+
+	cBuffer := (*C.char)(C.malloc(C.size_t(toRead)))
+	defer C.free(unsafe.Pointer(cBuffer))
+
+	cOffset := C.uint64_t(cfr.offset)
+
+	cToRead := C.uint64_t(toRead)
+
+	err := C.gp_camera_file_read(cfr.camera.camera, cFolderName, cFileName, C.GP_FILE_TYPE_NORMAL, cOffset, cBuffer, &cToRead, cfr.camera.context)
+	if err < 0 {
+		fmt.Printf("File Read Error: %s\n", CameraResultToString(int(err)))
+	}
+
+	amountRead := int(cToRead)
+
+	// Note: This is a double (triple?) buffering performance issue. It would be nice to write directly to the byte slice, but I'm not sure how to do that safely
+	bytes := C.GoBytes(unsafe.Pointer(cBuffer), C.int(amountRead))
+	for i, b := range bytes {
+		p[i] = b
+	}
+
+	cfr.offset += uint64(cToRead)
+
+	if cfr.offset < cfr.fullSize {
+		return amountRead, nil
+	} else {
+		return amountRead, io.EOF
+	}
+}
+
+func (c *Camera) FileReader(folder string, fileName string) io.Reader {
+	cfr := new(cameraFileReader)
+	cfr.camera = c
+	cfr.folder = folder
+	cfr.fileName = fileName
+	cfr.offset = 0
+
+	cFileName := C.CString(cfr.fileName)
+	cFolderName := C.CString(cfr.folder)
+	defer C.free(unsafe.Pointer(cFileName))
+	defer C.free(unsafe.Pointer(cFolderName))
+
+	var cCameraFileInfo C.CameraFileInfo
+
+	err := C.gp_camera_file_get_info(c.camera, cFolderName, cFileName, &cCameraFileInfo, c.context)
+	if err < 0 {
+		fmt.Printf("File Get Info Error: %s\n", CameraResultToString(int(err)))
+	}
+
+	cfr.fullSize = uint64(cCameraFileInfo.file.size)
+
+	return cfr
+}
+
+func (c *Camera) BufferedFileReader(folder string, fileName string) io.Reader {
+	return bufio.NewReaderSize(c.FileReader(folder, fileName), fileReaderBufferSize)
 }
